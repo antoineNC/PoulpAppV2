b04b7a31da4b4dede759b936dc0c15ea50a3b4a5
antoineNC <antoineney@hotmail.fr>
2023-08-07 22:10:18 +0200

    [design]boutons connexion

--- screens/connexion/logIn.tsx
+++ screens/connexion/logIn.tsx
@@ -1,21 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { Text, View, Image, TouchableOpacity, Button } from "react-native";
 import { LoginScreenNavProp } from "../../navigation/types";
-import * as AuthSession from "expo-auth-session";
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
-import { getUserInfo, refreshToken, revoke } from "../../service/googleAuth";
+import { getUserInfo } from "../../service/googleAuth";
 import firestoreService from "../../service/firestore.service";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import styles from "../../theme/styles";
+import { Icon } from "@rneui/themed";
 
 WebBrowser.maybeCompleteAuthSession();
 
 export default function LogIn({ navigation, route }: LoginScreenNavProp) {
-  const [userInfo, setUserInfo] = useState<{ name: string; email: string }>();
-  const [auth, setAuth] = useState<AuthSession.TokenResponse>();
-  const [requireRefresh, setRequireRefresh] = useState(false);
-
   const [request, response, promptAsync] = Google.useAuthRequest({
     androidClientId:
       "427196722560-bfottqft64l1bvmclt6hr11dhaejkcd3.apps.googleusercontent.com",
@@ -63,91 +59,63 @@ export default function LogIn({ navigation, route }: LoginScreenNavProp) {
       const authJsonValue = await AsyncStorage.getItem("auth");
       const userJsonValue = await AsyncStorage.getItem("sessionId");
       if (authJsonValue != null && userJsonValue != null) {
-        // const authFromJson = JSON.parse(authJsonValue);
-        // setAuth(authFromJson);
         navigation.navigate("Home", {
           screen: "FeedStack",
           params: { screen: "Feed" },
         });
-
-        // setRequireRefresh(
-        //   AuthSession.TokenResponse.isTokenFresh({
-        //     expiresIn: authFromJson.expiresIn,
-        //     issuedAt: authFromJson.issuedAt,
-        //   })
-        // );
       }
     };
     getPersistedAuth();
   }, []);
 
-  // const refresh = async () => {
-  //   const tokenResult = await refreshToken(auth);
-  //   setAuth(tokenResult);
-  //   await AsyncStorage.setItem("auth", JSON.stringify(tokenResult));
-  //   setRequireRefresh(false);
-  // };
-
-  // const logout = async () => {
-  //   if (auth?.accessToken) {
-  //     await revoke(auth);
-  //     setAuth(undefined);
-  //     setUserInfo(undefined);
-  //     await AsyncStorage.removeItem("auth");
-  //     console.log("logout auth: ", auth);
-  //     console.log("logout user: ", userInfo);
-  //   } else {
-  //     console.log("access token not available");
-  //   }
-  // };
-
   return (
     <View style={styles.mainContainer}>
       <View style={styles.container}>
+        <Text style={{ fontSize: 30, fontWeight: "bold" }}>POULP'APP</Text>
         <Image source={require("../../image/logo.png")} style={styles.logo} />
         {route.params !== undefined ? null : (
           <View style={{ marginVertical: 20 }}>
-            <Button
-              color={"#52234E"}
-              title="Acccéder à la session"
+            <TouchableOpacity
+              style={{
+                backgroundColor: "#52234E",
+                justifyContent: "center",
+                padding: 10,
+                borderRadius: 10,
+                flexDirection: "row",
+                columnGap: 10,
+                alignItems: "center",
+              }}
               onPress={() =>
                 navigation.navigate("Home", {
                   screen: "FeedStack",
                   params: { screen: "Feed" },
                 })
               }
-            />
+            >
+              <Text style={{ color: "#fff", fontWeight: "bold" }}>
+                ACCEDER A LA SESSION
+              </Text>
+              <Icon name="sign-in" type="font-awesome" color={"#fff"} />
+            </TouchableOpacity>
           </View>
         )}
-        {/* {route.params?.reconnect === false ? null : (
-          <View style={{ marginVertical: 20 }}>
-            <Button
-              title="Acccéder à la session"
-              onPress={() =>
-                navigation.navigate("Home", {
-                  screen: "FeedStack",
-                  params: { screen: "Feed" },
-                })
-              }
-            />
-          </View>
-        )} */}
-
-        <Button
-          color={"#52234E"}
-          title="Connectez-vous avec votre mail ENSC"
-          disabled={!request}
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#52234E",
+            padding: 10,
+            borderRadius: 10,
+            flexDirection: "row",
+            columnGap: 10,
+            alignItems: "center",
+          }}
           onPress={() => {
             promptAsync();
           }}
-        />
-
-        {/* {requireRefresh ? (
-          <View>
-            <Text>Token requires refresh...</Text>
-            <Button title="Refresh Token" onPress={refresh} />
-          </View>
-        ) : null} */}
+          disabled={!request}
+        >
+          <Text style={{ color: "#fff", fontWeight: "bold" }}>CONNEXION</Text>
+          <Icon name="google" type="antdesign" color={"#fff"} />
+        </TouchableOpacity>
 
         <Text style={{ marginTop: 10 }}>Pas encore de compte ?</Text>
         <TouchableOpacity onPress={() => navigation.navigate("Inscription")}>
