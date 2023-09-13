6200c3cae7a041960893cbd556326eff3dd86a69
antoineNC <antoineney@hotmail.fr>
2023-09-13 18:43:07 +0200

    Update config file

---
+++ service/config.js
@@ -0,0 +1,11 @@
+export const Firebase = {
+  APIKEY: "AIzaSyDcNbKtopB7Dxp2nGcYFlDiQ4TukEpoMPc",
+  messagingSenderId: "427196722560",
+};
+
+export const Google = {
+  iosId:
+    "139581308140-imf4dv4bogf4aj945eosqvnett4mp06e.apps.googleusercontent.com",
+  androidId:
+    "139581308140-n3ebiqnid8tmskvneo7lck2cku8va9s3.apps.googleusercontent.com",
+};
